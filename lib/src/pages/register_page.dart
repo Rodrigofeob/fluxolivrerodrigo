0f1d0139cbec57bd71f1f284eda8ff993b89efc9
Rodrigo <rodrigo.teixeira@sou.unifeob.edu.br>
2025-03-18 22:05:58 -0300

    Adicionando codigo

---
+++ lib/src/pages/register_page.dart
@@ -0,0 +1,86 @@
+import 'package:appfluxolivre/src/widgets/input_login_widget.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/img_fundologin.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(26.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 200,
+                width: 200,
+                child: Image(image: AssetImage('assets/images/et.png')),
+              ),
+              const SizedBox(height: 30),
+              InputLoginWidget(
+                icon: Icons.person_outline,
+                hint: 'Nome completo',
+                obscure: false,
+              ),
+              InputLoginWidget(
+                icon: Icons.email_outlined,
+                hint: 'Email',
+                obscure: false,
+              ),
+              InputLoginWidget(
+                icon: Icons.lock_outline,
+                hint: 'Senha',
+                obscure: true,
+              ),
+              InputLoginWidget(
+                icon: Icons.lock_outline,
+                hint: 'Confirmar senha',
+                obscure: true,
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFAFAE24),
+                  minimumSize: Size(double.infinity, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/home');
+                },
+                child: const Text(
+                  'Registrar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF031C5F),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+                child: const Text(
+                  'Já tenho uma conta',
+                  style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+} 
